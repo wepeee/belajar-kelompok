62d84066a21c3fe293ed564768828fc0aac9f967
Ryan Wiratara Prasetyo <ryanwpansal@gmail.com>
2024-09-14 23:50:06 +0700

    wepe push

--- coba.java
+++ coba.java
@@ -2,5 +2,6 @@ public class coba {
     public static void main(String[] args) {
         System.out.println("File siap diedit");
         System.out.println("file early");
+        System.out.println("tambahan dari wepe");
     }
 }
