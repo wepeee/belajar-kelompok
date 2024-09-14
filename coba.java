4fa2e37e6bd4d55f5f5c732fa2aea56eacbb9123
EarlyyChel <earlyyychelsealia@gmail.com>
2024-09-14 23:38:08 +0700

    fitur tambah file early

--- coba.java
+++ coba.java
@@ -1,5 +1,6 @@
-public class coba{
+public class coba {
     public static void main(String[] args) {
         System.out.println("File siap diedit");
+        System.out.println("file early");
     }
 }
